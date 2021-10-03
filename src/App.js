64dc436614c0b9aa6f56938921967b34a9ab854e
Supriya <supriya1911033@akgec.ac.in>
2021-10-02 19:18:03 -0700

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
+import React from "react";
+import { Switch , Route } from 'react-router-dom'
+
 
 
 function App() {
   return (
-    <div >
-    </div>
+    <Switch>
+      <Route exact path="/">
+        This is home page
+
+      </Route>
+      <Route exact path="/starred">
+        This is starred
+
+      </Route> 
+      <Route>This is 404 page</Route>
+
+    </Switch>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render( <App />,
+ReactDOM.render(<BrowserRouter>
+ <App />
+ </BrowserRouter>,
   document.getElementById('root')
 );
 
